b3b99647c2b0089b370ba26c635e7f8ae3a40ff8
Rolando <rolando_osuna@live.com.mx>
2022-06-08 02:11:09 -0600

    evento click en desayuno

---
+++ JS/main.js
@@ -0,0 +1,30 @@
+let boton = document.querySelector('.boton-cards');
+let cards = document.querySelector('.cards');
+boton.addEventListener('click', element => {
+  cards.classList.toggle('contenido-platillos')
+}) 
+let boton2 = document.querySelector('.boton-cards2');
+let cards2 = document.querySelector('.cards2');
+boton2.addEventListener('click', element => {
+  cards2.classList.toggle('contenido-platillos2')
+}) 
+let boton3 = document.querySelector('.boton-cards3');
+let cards3 = document.querySelector('.cards3');
+boton3.addEventListener('click', element => {
+  cards3.classList.toggle('contenido-platillos3')
+}) 
+let boton4 = document.querySelector('.boton-cards4');
+let cards4 = document.querySelector('.cards4');
+boton4.addEventListener('click', element => {
+  cards4.classList.toggle('contenido-platillos4')
+}) 
+let boton5 = document.querySelector('.boton-cards5');
+let cards5 = document.querySelector('.cards5');
+boton5.addEventListener('click', element => {
+  cards5.classList.toggle('contenido-platillos5')
+}) 
+let boton6 = document.querySelector('.boton-cards6');
+let cards6 = document.querySelector('.cards6');
+boton6.addEventListener('click', element => {
+  cards6.classList.toggle('contenido-platillos6')
+}) 
